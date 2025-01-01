3269a4d4ca5f1fa81d2de90f72ace6667e8e63f0
ooooose <yuuki.oose1990@gmail.com>
2025-01-01 16:07:21 +0900

    Add middlewareでCORS設定を行なった

--- back/app/main.go
+++ back/app/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"net/http"
 	"todo/db"
 	"todo/routes"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	db.Init()
 
 	server := echo.New()
+	server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{http.MethodGet	, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
+
 	routes.RegisterRoutes(server)
 
 	server.Logger.Fatal(server.Start(":5050"))
