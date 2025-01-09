57660d62ca6365538d3ee761f350aa3e51e19f9e
ooooose <yuuki.oose1990@gmail.com>
2025-01-09 22:22:45 +0900

    Fix name修正

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsTaskExecution"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -16,6 +16,6 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role = aws_iam_role.ecs_task_execution_role.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
